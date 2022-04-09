1fa42c9d1a4d7251c71ebcf3f14a1a159841fc4a
fakhrinail <fakhrinailwbw@gmail.com>
2022-04-09 17:28:37 +0700

    setup recycler view

--- app/src/main/java/com/bangkit/storyapp/view/main/MainActivity.kt
+++ app/src/main/java/com/bangkit/storyapp/view/main/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.bangkit.storyapp.view.main
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
 import com.bangkit.storyapp.R
+import com.bangkit.storyapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        setupView()
+    }
+
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
     }
 }
