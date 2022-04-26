c92f435e5e4f10483e1c37a6460e53544affa7a1
fakhrinail <fakhrinailwbw@gmail.com>
2022-04-26 19:05:17 +0700

    add testing login

---
+++ app/src/test/java/com/bangkit/storyapp/view/login/LoginViewModelTest.kt
@@ -0,0 +1,52 @@
+package com.bangkit.storyapp.view.login
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import com.bangkit.storyapp.data.Result
+import com.bangkit.storyapp.model.user.UserModel
+import com.bangkit.storyapp.util.DataDummy
+import com.bangkit.storyapp.util.getOrAwaitValue
+import org.junit.Assert
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class LoginViewModelTest {
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    @Mock
+    private lateinit var loginViewModel: LoginViewModel
+
+    private var dummyEmail: String? = null
+    private var dummyPassword: String? = null
+    private val dummyUser = DataDummy.generateDummyUserModel()
+
+    @Test
+    fun `when Login Input Should Not Null and Success`() {
+        dummyEmail = "test@gmail.com"
+        dummyPassword = "test123"
+
+        val expectedUser = MutableLiveData<Result<UserModel>>()
+        expectedUser.value = Result.Success(dummyUser)
+
+        Mockito.`when`(loginViewModel.login(dummyEmail!!, dummyPassword!!)).thenReturn(expectedUser)
+
+        val actualUser = loginViewModel.login(dummyEmail!!, dummyPassword!!).getOrAwaitValue()
+
+        Assert.assertNotNull(actualUser)
+        Assert.assertTrue(actualUser is Result.Success)
+        Assert.assertEquals(dummyUser.userId, (actualUser as Result.Success).data.userId)
+    }
+
+    @Test(expected = NullPointerException::class)
+    fun `when Login Input Null and Exception Thrown`() {
+        Mockito.`when`(loginViewModel.login(dummyEmail!!, dummyPassword!!)).thenThrow(NullPointerException())
+
+        loginViewModel.login(dummyEmail!!, dummyPassword!!).getOrAwaitValue()
+    }
+}
