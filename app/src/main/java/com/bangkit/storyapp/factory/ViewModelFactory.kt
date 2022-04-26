35d94dbf56bad287ba934a3ac39ef568ab74a0d0
fakhrinail <fakhrinailwbw@gmail.com>
2022-04-26 21:46:53 +0700

    update ViewModelFactory.kt

--- app/src/main/java/com/bangkit/storyapp/factory/ViewModelFactory.kt
+++ app/src/main/java/com/bangkit/storyapp/factory/ViewModelFactory.kt
@@ -4,8 +4,10 @@ import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.bangkit.storyapp.di.Injection
+import com.bangkit.storyapp.view.login.LoginViewModel
 import com.bangkit.storyapp.view.main.MainViewModel
 import com.bangkit.storyapp.view.maps.MapsViewModel
+import com.bangkit.storyapp.view.register.RegisterViewModel
 
 class ViewModelFactory(private val context: Context) : ViewModelProvider.NewInstanceFactory() {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
@@ -18,6 +20,16 @@ class ViewModelFactory(private val context: Context) : ViewModelProvider.NewInst
             @Suppress("UNCHECKED_CAST")
             with(Injection) {
                 return MapsViewModel(provideRepository(context)) as T
+            }
+        } else if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            with(Injection) {
+                return LoginViewModel(provideService(context)) as T
+            }
+        } else if (modelClass.isAssignableFrom(RegisterViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            with(Injection) {
+                return RegisterViewModel(provideService(context)) as T
             }
         }
 
