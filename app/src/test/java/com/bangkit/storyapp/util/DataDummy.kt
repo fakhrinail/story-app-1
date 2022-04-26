55106471ef373b1ee5737c97dd03aa89da3cea46
fakhrinail <fakhrinailwbw@gmail.com>
2022-04-26 21:33:20 +0700

    add new data dummy functions

--- app/src/test/java/com/bangkit/storyapp/util/DataDummy.kt
+++ app/src/test/java/com/bangkit/storyapp/util/DataDummy.kt
@@ -1,25 +1,29 @@
 package com.bangkit.storyapp.util
 
 import com.bangkit.storyapp.model.ApiResponse
+import com.bangkit.storyapp.model.login.LoginRequest
+import com.bangkit.storyapp.model.login.LoginResult
 import com.bangkit.storyapp.model.story.ListStoryItem
 import com.bangkit.storyapp.model.user.UserModel
 
 object DataDummy {
-    fun generateDummySuccessResponseModel(): ApiResponse {
-        return ApiResponse(false, "success")
-    }
+    fun generateDummySuccessResponseModel(): ApiResponse = ApiResponse(false, "success")
 
-    fun generateDummyFailedResponseModel(): ApiResponse {
-        return ApiResponse(true, "Error message")
-    }
+    fun generateDummyLoginSuccessResponseModel(): ApiResponse = ApiResponse(false, "success", LoginResult(
+         "fakhri",
+        "dummy-id",
+         "dummy-token"
+    ))
 
-    fun generateDummyUserModel(): UserModel {
-        return UserModel(
-            name = "Fakhri Nail",
-            token = "test123initoken",
-            userId = "userid123iniuserid"
-        )
-    }
+    fun generateDummyFailedResponseModel(): ApiResponse = ApiResponse(true, "Error message")
+
+    fun generateDummyLoginRequestModel(): LoginRequest = LoginRequest("test@gmail.com", "test123")
+
+    fun generateDummyUserModel(): UserModel = UserModel(
+        name = "Fakhri Nail",
+        token = "test123initoken",
+        userId = "userid123iniuserid"
+    )
 
     fun generateDummyStoryResponse(): List<ListStoryItem> {
         val items: MutableList<ListStoryItem> = arrayListOf()
